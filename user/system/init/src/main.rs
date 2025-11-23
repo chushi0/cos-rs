76c4a2070e42a59490c15ac137dcaa7914fa004a
初始状态0 <chushi0@users.noreply.github.com>
2025-11-23 19:35:27 +0800

    system application

---
+++ user/system/init/src/main.rs
@@ -0,0 +1,19 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(test_runner)]
+
+use cos_sys::multitask::exit;
+
+#[unsafe(export_name = "_start")]
+fn main() -> ! {
+    exit(0);
+}
+
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    exit(3);
+}
+
+#[cfg(test)]
+fn test_runner(_test_cases: &[&dyn Fn()]) {}
